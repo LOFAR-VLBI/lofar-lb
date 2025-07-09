e4f8eadfd7b50d0039cb8c28ca0ede7cb20f171c
Neal Jackson <neal.jackson@manchester.ac.uk>
2025-07-09 18:02:57 +0100

    Add files via upload

--- h5movie.py
+++ h5movie.py
@@ -1,13 +1,16 @@
-import os,glob,numpy as np,aplpy,matplotlib,h5py
+import os,glob,numpy as np,matplotlib,h5py
 from matplotlib import pyplot as plt
 from astropy.time import Time
+from astropy.visualization.wcsaxes import SphericalCircle
+from astropy.coordinates import SkyCoord
 import astropy.io.fits as pyfits; from astropy import units as u
-H5PATTERN = '*_sols.h5'
+from astropy.wcs import WCS
 ANTPLOT = 'DE605'
-ANTREF = 'ST001'
+ANTREF = 'CS001'
 #LBCSFILE = 'lbcs_stats.sum'
 #LBCSHTTP = 'http://www.jb.man.ac.uk/~njj/lbcs_stats.sum'
 POL = 0
+CHAN = 50
 
 def movie (title='zoom',fps=4):
 #   ---- commented out (gives higher quality but no mencoder available ----
@@ -17,7 +20,9 @@ def movie (title='zoom',fps=4):
 #    command = "mencoder \"mf://%s*.jpeg\" -mf fps=%d -o %s.avi -ovc lavc -lavcopts vcodec=mpeg4:vbitrate=4800" %(title,fps,title)
 #    os.system(command)
 #   ---- use convert instead ------------
-    os.system('convert '+title+'*.png h5movie.mpg')
+#    os.system('convert '+title+'*.png h5movie.mpg')
+#ffmpeg -framerate 4 -pattern_type glob -i 'zoom*.jpg' -c:v libx264 -pix_fmt yuv420p montage.mp4
+    pass
 
 def getpcol(phase):
     phase = 3.0*(phase%(2*np.pi))/(2.*np.pi)
@@ -43,8 +48,12 @@ def mkfits (rasiz,decsiz,imsiz,pixsiz):
     os.system('rm temp.fits')
     hdu.writeto('temp.fits')
 
+def get_h5list ():   # adjust for the system used
+    os.system('find * -print |grep merged|grep h5 >temp')
+    h5files = np.loadtxt('temp',dtype='str')
+    return np.sort(h5files)
 
-h5list = np.sort(glob.glob(H5PATTERN))
+h5list = get_h5list()
 #try:
 #    lbcs = np.loadtxt(LBCSFILE,dtype='str')
 #except:
@@ -54,15 +63,15 @@ h5list = np.sort(glob.glob(H5PATTERN))
 pos=np.array([]);phase=np.array([]);ut=np.array([])
 for h5 in h5list:
     h = h5py.File(h5)
-    newpos = np.asarray(h['sol000/source'].value[0][1],dtype='float')
+    newpos = np.asarray(h['sol000/source'][0][1],dtype='float')
     ant = np.array(h['sol000/antenna'])
     for i in range(len(ant)):
         if str(ant[i][0],'utf8')[:5] == ANTPLOT:
             idxant = i
         if str(ant[i][0],'utf8')[:5] == ANTREF:
             idxref = i
-    newphase = np.asarray(h['sol000/phase000/val'],dtype='float')[:,0,idxant,POL]
-    newref = np.asarray(h['sol000/phase000/val'],dtype='float')[:,0,idxref,POL]
+    newphase = np.asarray(h['sol000/phase000/val'],dtype='float')[:,CHAN,idxant,POL,0]
+    newref = np.asarray(h['sol000/phase000/val'],dtype='float')[:,CHAN,idxref,POL,0]
     newut = np.asarray(h['sol000/phase000']['time'],dtype='float')/86400.0
     try:
         ut = np.vstack((ut,newut))
@@ -89,15 +98,37 @@ width = 1.5*max(decwidth,rawidth*np.cos(dec))
 mkfits (ra,dec,600,width/600.0)
 #for nframe in range(phase.shape[1]):
 os.system('rm zoom*.png;rm zoom*jpeg')
+print(phase.shape)
 for nframe in range(min(phase.shape[1],500)):
-    gc = aplpy.FITSFigure('temp.fits')
-    gc.add_grid()
-    gc.grid.set_color('black')
+    print('Frame %d'%nframe)
+    hdu = pyfits.open('temp.fits')[0]
+    ax = plt.subplot(1,1,1,xticks=[],yticks=[],projection=WCS(hdu.header))
+    plt.imshow(pyfits.getdata('temp.fits'),cmap=matplotlib.cm.gray_r)
+#    ax.coords[0].set_axislabel(False)
+#    ax.coords[1].set_axislabel(False)
+#    ax.coords[0].set_ticks_visible(False)
+#    ax.coords[1].set_ticklabel_visible(False)
+#    ax.coords[0].set_ticklabel_visible(False)
+#    ax.coords[1].set_ticks_visible(False)
     for i in range(len(phase)):
-        gc.show_circles(pos[i,0],pos[i,1],0.1,color='k',\
-                    facecolor=getpcol(phase[i,nframe]))
-    gc.set_title(Time(ut[0,nframe],format='mjd').fits)
-    gc.save('zoom%06d.png'%nframe)
-    gc.close()
+        ax.scatter(pos[i,0],pos[i,1],s=100,facecolor=getpcol(phase[i,nframe]),transform=ax.get_transform('fk5'))
+#        thiscoord = SkyCoord(pos[i,0]*u.deg,pos[i,1]*u.deg)
+#        print (thiscoord,phase[i,nframe],getpcol(phase[i,nframe]))
+#        ell = SphericalCircle (thiscoord,0.01*u.deg,\
+#                               facecolor = getpcol(phase[i,nframe]))
+#    ax.add_artist(ell)
+    plt.title(ANTPLOT)
+    plt.savefig('zoom%06d.png'%nframe)
+    plt.clf()
 
-movie (title='zoom',fps=10)
+#    gc = aplpy.FITSFigure('temp.fits')
+#    gc.add_grid()
+#    gc.grid.set_color('black')
+#    for i in range(len(phase)):
+#        gc.show_circles(pos[i,0],pos[i,1],0.1,color='k',\
+#                    facecolor=getpcol(phase[i,nframe]))
+#    gc.set_title(Time(ut[0,nframe],format='mjd').fits)
+#    gc.save('zoom%06d.png'%nframe)
+#    gc.close()
+
+#movie (title='zoom',fps=10)
